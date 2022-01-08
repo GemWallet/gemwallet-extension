5b87700afb248da40a9c5872151f750d19015e69
Florian Bouron <bouron.florian@yahoo.fr>
2022-01-08 21:33:29 +0100

    Add share button

--- src/components/molecules/Wallet/Wallet.tsx
+++ src/components/molecules/Wallet/Wallet.tsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from 'react';
+import copyToClipboard from 'copy-to-clipboard';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import ShareIcon from '@mui/icons-material/Share';
 import { useLedger } from '../../../contexts/LedgerContext';
 import { PropType } from './Wallet.types';
 
 export function Wallet({ address = '' }: PropType) {
   const [balance, setBalance] = useState('Loading');
+  const [isShared, setIsShared] = useState(false);
   const { client } = useLedger();
 
   useEffect(() => {
@@ -21,9 +26,28 @@ export function Wallet({ address = '' }: PropType) {
     fetchBalance();
   }, [address, client]);
 
+  const handleShare = () => {
+    copyToClipboard(address);
+    setIsShared(true);
+    setTimeout(() => setIsShared(false), 5000);
+  };
+
   return (
     <Paper elevation={24} style={{ padding: '10px' }}>
-      <Typography variant="h5">Wallet</Typography>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography variant="h5">Wallet</Typography>
+        <Tooltip title="Share your public address">
+          <IconButton
+            size="small"
+            edge="end"
+            color="inherit"
+            aria-label="Copy"
+            onClick={handleShare}
+          >
+            {isShared ? <ShareIcon color="success" /> : <ShareIcon />}
+          </IconButton>
+        </Tooltip>
+      </div>
       <Typography variant="body2" style={{ margin: '10px 0' }}>
         {address}
       </Typography>
