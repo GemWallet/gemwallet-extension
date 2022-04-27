462a9d04322859c478cacde48dc471fdff393052
Florian Bouron <bouron.florian@yahoo.fr>
2022-04-27 20:41:56 +0200

    Add Login page test

---
+++ packages/extension/src/components/pages/Login/Login.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Login } from '.';
+
+describe('Login Page', () => {
+  test('Should render the proper elements', () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
+    const titleElement = screen.getByRole('heading', { name: 'GemWallet' });
+    const subTitleElement = screen.getByRole('heading', {
+      name: 'Internet cryptocurrency payments made easy'
+    });
+    expect(titleElement).toBeVisible();
+    expect(subTitleElement).toBeVisible();
+  });
+});
