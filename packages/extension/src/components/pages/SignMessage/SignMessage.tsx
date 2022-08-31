d91d156f2586c14bce76a65ed0caa6275ff54e8b
Florian Bouron <bouron.florian@yahoo.fr>
2022-08-31 21:16:36 +0200

    Add proper message within the SignMessage step

--- packages/extension/src/components/pages/SignMessage/SignMessage.tsx
+++ packages/extension/src/components/pages/SignMessage/SignMessage.tsx
@@ -114,15 +114,7 @@ export const SignMessage: FC = () => {
         <Divider style={{ margin: '10px 0' }} />
         <div style={{ overflowY: 'scroll', height: '200px' }}>
           <Typography variant="body2" style={{ color: SECONDARY_GRAY }}>
-            I am going to do it. I have made up my mind. These are the first few words of the new…
-            the best … the Longest Text In The Entire History Of The Known Universe! This Has To
-            Have Over 35,000 words the beat the current world record set by that person who made
-            that flaming chicken handbooky thingy. I might just be saying random things the whole
-            time I type in this so you might get confused a lot. I just discovered something
-            terrible. autocorrect is on!! no!!! this has to be crazy, so I will have to break all
-            the English language rules and the basic knowledge of the average human being. I am not
-            an average human being, however I am special. no no no, not THAT kind of special ;). Why
-            do
+            {message}
           </Typography>
         </div>
       </Paper>
