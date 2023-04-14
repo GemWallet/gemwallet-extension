42dd3b2fcee49ca817008449a61a6229b5d113ba
Florian <bouron.florian@yahoo.fr>
2023-04-14 12:20:41 +0200

    Fix HEX issue within the send payment option (#131)

--- packages/extension/src/components/pages/SendPayment/PreparePayment/PreparePayment.tsx
+++ packages/extension/src/components/pages/SendPayment/PreparePayment/PreparePayment.tsx
@@ -14,6 +14,7 @@ import { useNavigate } from 'react-router-dom';
 import { isValidAddress } from 'xrpl';
 
 import { useNetwork, useWallet } from '../../../../contexts';
+import { convertCurrencyString } from '../../../../utils';
 import { NumericInput } from '../../../atoms';
 import { InformationMessage } from '../../../molecules';
 import { PageWithReturn, PageWithSpinner } from '../../../templates';
@@ -194,7 +195,7 @@ export const PreparePayment: FC<PreparePaymentProps> = ({ onSendPaymentClick }) 
                 key={`${token.currency}-${token.issuer}`}
                 value={`${token.currency}-${token.issuer}`}
               >
-                {token.currency}
+                {convertCurrencyString(token.currency)}
               </MenuItem>
             ))}
           </Select>
