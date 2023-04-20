aa02be1922c084781d9a18a6cbb3c06c9807e2f3
thibautbremand <thibaut.bremand@gmail.com>
2023-04-20 18:30:34 +0200

    Add unit tests: payment.test.ts

---
+++ packages/extension/src/utils/payment.test.ts
@@ -0,0 +1,25 @@
+import { DEFAULT_MEMO_TYPE } from '@gemwallet/constants';
+
+import { buildMemos } from './payment';
+
+describe('buildMemos', () => {
+  test('returns undefined when memoData is undefined or an empty string', () => {
+    expect(buildMemos(undefined)).toBeUndefined();
+    expect(buildMemos('')).toBeUndefined();
+  });
+
+  test('returns an array with Memo object when memoData is a non-empty string', () => {
+    const memoData = 'Test Memo';
+
+    const expectedResult = [
+      {
+        Memo: {
+          MemoType: Buffer.from(DEFAULT_MEMO_TYPE, 'utf8').toString('hex').toUpperCase(),
+          MemoData: Buffer.from(memoData, 'utf8').toString('hex').toUpperCase()
+        }
+      }
+    ];
+
+    expect(buildMemos(memoData)).toEqual(expectedResult);
+  });
+});
