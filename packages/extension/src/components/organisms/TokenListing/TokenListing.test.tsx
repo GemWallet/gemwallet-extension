e5a496e1c2e146d4b1736e972ca82e122ec25782
thibautbremand <thibaut.bremand@gmail.com>
2023-04-24 16:16:24 +0200

    Fix tests: TokenListing.test.tsx

--- packages/extension/src/components/organisms/TokenListing/TokenListing.test.tsx
+++ packages/extension/src/components/organisms/TokenListing/TokenListing.test.tsx
@@ -1,5 +1,6 @@
 import * as Sentry from '@sentry/react';
 import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import { TokenListing, TokenListingProps } from './TokenListing';
 
@@ -54,7 +55,7 @@ describe('TokenListing', () => {
 
   test('should display an error when client failed to load', () => {
     mockClient = null;
-    render(<TokenListing {...props} />);
+    render(<MemoryRouter><TokenListing {...props} /></MemoryRouter>);
     expect(
       screen.queryByText(
         'There was an error attempting to retrieve your assets. Please refresh and try again.'
@@ -63,7 +64,7 @@ describe('TokenListing', () => {
   });
 
   test('should display the loading token state when the XRPBalance is not calculated', () => {
-    render(<TokenListing {...props} />);
+    render(<MemoryRouter><TokenListing {...props} /></MemoryRouter>);
     expect(screen.getByTestId('token-loader')).toBeInTheDocument();
   });
 
@@ -73,7 +74,7 @@ describe('TokenListing', () => {
       { value: '50', currency: 'USD', issuer: 'r123' },
       { value: '20', currency: 'ETH', issuer: 'r456' }
     ]);
-    render(<TokenListing {...props} />);
+    render(<MemoryRouter><TokenListing {...props} /></MemoryRouter>);
     await waitFor(() => {
       expect(screen.getByText('90 XRP')).toBeInTheDocument();
       expect(screen.getByText('50 USD')).toBeInTheDocument();
@@ -85,7 +86,7 @@ describe('TokenListing', () => {
     mockGetBalancesPromise.mockRejectedValueOnce(
       new Error('Throw an error if there is an error fetching the balances')
     );
-    render(<TokenListing {...props} />);
+    render(<MemoryRouter><TokenListing {...props} /></MemoryRouter>);
     await waitFor(() => {
       expect(screen.getByText('Account not activated')).toBeVisible();
       expect(Sentry.captureException).toHaveBeenCalled();
@@ -96,7 +97,7 @@ describe('TokenListing', () => {
     mockGetBalancesPromise.mockResolvedValueOnce([
       { value: '100', currency: 'XRP', issuer: undefined }
     ]);
-    render(<TokenListing {...props} />);
+    render(<MemoryRouter><TokenListing {...props} /></MemoryRouter>);
     const explainButton = await screen.findByText('Explain');
     await fireEvent.click(explainButton);
     expect(
