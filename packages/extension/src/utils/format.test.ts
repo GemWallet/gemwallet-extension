ad34723913571a10aaf841d98d6d3331e7c28246
Florian Bouron <bouron.florian@yahoo.fr>
2023-05-11 17:58:52 +0200

    WIP

--- packages/extension/src/utils/format.test.ts
+++ packages/extension/src/utils/format.test.ts
@@ -1,6 +1,6 @@
 import { xrpToDrops } from 'xrpl';
 
-import { formatAmount } from './format';
+import { formatAmount, formatToken } from './format';
 
 describe('Format util', () => {
   describe('formatAmount', () => {
@@ -21,6 +21,50 @@ describe('Format util', () => {
         issuer: 'fake'
       };
       expect(formatAmount(amount)).toEqual('1,234,567.8977 USD');
+    });
+  });
+
+  describe('formatToken', () => {
+    test('should return 1,234,567 XRP', () => {
+      const value = 1234567;
+      const currency = 'XRP';
+      const isDrops = false;
+      expect(formatToken(value, currency, isDrops)).toEqual('1,234,567 XRP');
+    });
+
+    test('should return 1.234567 XRP', () => {
+      const value = 1234567;
+      const currency = 'XRP';
+      const isDrops = true;
+      expect(formatToken(value, currency, isDrops)).toEqual('1.234567 XRP');
+    });
+
+    test('should return 1,234,567.8977 XRP', () => {
+      const value = 1234567.8977;
+      const currency = 'XRP';
+      const isDrops = false;
+      expect(formatToken(value, currency, isDrops)).toEqual('1,234,567.8977 XRP');
+    });
+
+    test('should return 1.2345678977 XRP', () => {
+      const value = 1234567.8977;
+      const currency = 'XRP';
+      const isDrops = true;
+      expect(formatToken(value, currency, isDrops)).toEqual('1.2345678977 XRP');
+    });
+
+    test('should return 1,234,567.8977 USD', () => {
+      const value = 1234567.8977;
+      const currency = 'USD';
+      const isDrops = false;
+      expect(formatToken(value, currency, isDrops)).toEqual('1,234,567.8977 USD');
+    });
+
+    test('should return 1,234,567.89779 USD', () => {
+      const value = 1234567.8977;
+      const currency = 'USD';
+      const isDrops = true;
+      expect(formatToken(value, currency, isDrops)).toEqual('1,234,567.8977 USD');
     });
   });
 });
