a11ad03d028a198ff08ddd2510f48e4eb5a1dda1
Florian Bouron <bouron.florian@yahoo.fr>
2023-06-17 07:24:32 +0200

    Extract the currencyToHex to another file

--- packages/extension/src/utils/crypto.test.ts
+++ packages/extension/src/utils/crypto.test.ts
@@ -1,4 +1,4 @@
-import { encrypt, decrypt, currencyToHex } from './crypto';
+import { encrypt, decrypt } from './crypto';
 
 const message = 'My secret message';
 // deepcode ignore NoHardcodedPasswords/test: This password is used only for unit-testing
@@ -19,15 +19,5 @@ describe('Crypto util', () => {
 
       expect(decryptedMessage).toEqual('');
     });
-  });
-});
-
-describe('currencyToHex function', () => {
-  it('should convert a string into hex and pad it to 40 characters', () => {
-    const currency = 'SOLO';
-    const hex = currencyToHex(currency);
-
-    expect(hex).toEqual('534F4C4F00000000000000000000000000000000');
-    expect(hex.length).toEqual(40);
   });
 });
