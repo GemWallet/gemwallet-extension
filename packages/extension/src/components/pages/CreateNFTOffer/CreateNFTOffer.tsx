88396c074b294c52e09868896618ae9a9a73d195
Florian Bouron <bouron.florian@yahoo.fr>
2023-06-22 22:08:51 +0200

    Fix styling

--- packages/extension/src/components/pages/CreateNFTOffer/CreateNFTOffer.tsx
+++ packages/extension/src/components/pages/CreateNFTOffer/CreateNFTOffer.tsx
@@ -225,7 +225,10 @@ export const CreateNFTOffer: FC = () => {
       {transactionStatusComponent ? (
         <div>{transactionStatusComponent}</div>
       ) : (
-        <PageWithTitle title="Confirm Transaction">
+        <PageWithTitle
+          title="Confirm Transaction"
+          styles={{ container: { justifyContent: 'initial' } }}
+        >
           {!hasEnoughFunds ? (
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
               <ErrorIcon style={{ color: ERROR_RED }} />
