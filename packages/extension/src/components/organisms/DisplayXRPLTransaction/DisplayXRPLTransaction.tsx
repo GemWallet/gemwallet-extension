d8a6345145953afdce46df712cf56a630e46ed5d
Florian Bouron <bouron.florian@yahoo.fr>
2023-07-03 23:01:17 +0200

    Add types within DisplayXRPLTransaction

--- packages/extension/src/components/organisms/DisplayXRPLTransaction/DisplayXRPLTransaction.tsx
+++ packages/extension/src/components/organisms/DisplayXRPLTransaction/DisplayXRPLTransaction.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import { Paper, Tooltip, Typography } from '@mui/material';
 import { convertHexToString, Transaction } from 'xrpl';
-import { Amount, Memo } from 'xrpl/dist/npm/models/common';
+import { Amount, Memo, Signer } from 'xrpl/dist/npm/models/common';
 import { GlobalFlags } from 'xrpl/dist/npm/models/transactions/common';
 
 import { formatAmount, formatFlags, formatTransferFee } from '../../../utils';
@@ -12,30 +12,41 @@ type XRPLTxProps = {
 };
 
 export const DisplayXRPLTransaction: FC<XRPLTxProps> = ({ tx }) => {
-  const keyMap: Record<string, (value: unknown) => JSX.Element> = {
-    TransactionType: (value) => renderSimpleText('Transaction Type', value),
-    Amount: (value) => renderAmount('Amount', value as Amount),
-    NFTokenID: (value) => renderSimpleText('NFT Token ID', value),
-    Destination: (value) => renderSimpleText('Destination', value),
-    DestinationTag: (value) => renderSimpleText('Destination Tag', value),
-    Flags: (value) => renderSimpleText('Flags', formatFlags(value as GlobalFlags)),
-    Memos: (value) => renderMemos(value as Memo[]),
-    NFTokenOffers: (value) => renderArray('Offer IDs', value),
-    Signers: (value) => renderArray('Signers', value),
+  const keyMap: Record<string, (value: any) => JSX.Element | null> = {
+    TransactionType: (value: string) => renderSimpleText('Transaction Type', value),
+    Amount: (value: Amount) => renderAmount('Amount', value),
+    NFTokenID: (value: string) => renderSimpleText('NFT Token ID', value),
+    Destination: (value: string) => renderSimpleText('Destination', value),
+    DestinationTag: (value?: number) => renderSimpleText('Destination Tag', value),
+    Flags: (value?: GlobalFlags) =>
+      value !== undefined ? renderSimpleText('Flags', formatFlags(value)) : null,
+    Memos: (value?: Memo[]) => renderMemos(value),
+    NFTokenOffers: (value: string[]) => renderArray('Offer IDs', value),
+    Signers: (value?: Signer[]) => renderArray('Signers', value),
     LimitAmount: (value) => renderAmount('Limit Amount', value as Amount),
-    NFTokenSellOffer: (value) => renderSimpleText('NFT Token Sell Offer', value),
-    NFTokenBuyOffer: (value) => renderSimpleText('NFT Token Buy Offer', value),
-    NFTokenBrokerFee: (value) => renderAmount('NFT Token Broker Fee', value as Amount),
-    NFTokenMinter: (value) => renderSimpleText('NFT Token Minter', value),
-    URI: (value) => renderSimpleText('URI', value ? convertHexToString(value as string) : '', true),
-    Fee: () => <></>, // Fee is rendered in the BaseTransaction component
-    TakerGets: (value) => renderAmount('Taker Gets', value as Amount),
-    TakerPays: (value) => renderAmount('Taker Pays', value as Amount),
-    TransferFee: (value) =>
-      renderSimpleText('Transfer Fee', value ? `${formatTransferFee(value as number)}%` : '')
+    NFTokenSellOffer: (value?: string) =>
+      value !== undefined ? renderSimpleText('NFT Token Sell Offer', value) : null,
+    NFTokenBuyOffer: (value?: string) =>
+      value !== undefined ? renderSimpleText('NFT Token Buy Offer', value) : null,
+    NFTokenBrokerFee: (value?: Amount) =>
+      value !== undefined ? renderAmount('NFT Token Broker Fee', value) : null,
+    NFTokenMinter: (value?: string) =>
+      value !== undefined ? renderSimpleText('NFT Token Minter', value) : null,
+    URI: (value?: string | null) =>
+      value !== undefined
+        ? renderSimpleText('URI', value ? convertHexToString(value as string) : '', true)
+        : null,
+    Fee: () => null, // Fee is rendered in the BaseTransaction component
+    TakerGets: (value: Amount) => renderAmount('Taker Gets', value),
+    TakerPays: (value: Amount) => renderAmount('Taker Pays', value),
+    TransferFee: (value?: number) =>
+      renderSimpleText('Transfer Fee', value ? `${formatTransferFee(value)}%` : '')
   };
 
-  const renderSimpleText = (title: string, value: unknown, tooltip?: boolean) => {
+  const renderSimpleText = (title: string, value: any, tooltip?: boolean): JSX.Element | null => {
+    if (value === undefined) {
+      return null;
+    }
     const strValue = String(value);
     const typo = (
       <Typography
@@ -67,28 +78,33 @@ export const DisplayXRPLTransaction: FC<XRPLTxProps> = ({ tx }) => {
     </Paper>
   );
 
-  const renderMemos = (memos: Memo[]) => (
-    <Paper elevation={24} style={{ padding: '10px', marginBottom: '5px' }}>
-      <Typography variant="body1">Memos:</Typography>
-      {memos.map((memo, index) => (
-        <div key={index} style={{ marginBottom: index === memos.length - 1 ? 0 : '8px' }}>
-          <Typography
-            variant="body2"
-            style={{
-              whiteSpace: 'nowrap',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              maxWidth: '100%'
-            }}
-          >
-            {memo.Memo.MemoData
-              ? Buffer.from(memo.Memo.MemoData, 'hex').toString('utf8')
-              : memo.Memo.MemoData}
-          </Typography>
-        </div>
-      ))}
-    </Paper>
-  );
+  const renderMemos = (memos?: Memo[]) => {
+    if (memos === undefined) {
+      return null;
+    }
+    return (
+      <Paper elevation={24} style={{ padding: '10px', marginBottom: '5px' }}>
+        <Typography variant="body1">Memos:</Typography>
+        {memos.map((memo, index) => (
+          <div key={index} style={{ marginBottom: index === memos.length - 1 ? 0 : '8px' }}>
+            <Typography
+              variant="body2"
+              style={{
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                maxWidth: '100%'
+              }}
+            >
+              {memo.Memo.MemoData
+                ? Buffer.from(memo.Memo.MemoData, 'hex').toString('utf8')
+                : memo.Memo.MemoData}
+            </Typography>
+          </div>
+        ))}
+      </Paper>
+    );
+  };
 
   const renderArray = (title: string, value: unknown) => {
     if (!Array.isArray(value)) {
