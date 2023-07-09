67ace157eb1e343a7d22b8e6dcfc7ea75a55b358
Florian Bouron <bouron.florian@yahoo.fr>
2023-07-09 18:37:57 +0200

    Remove strings

--- packages/extension/src/components/organisms/BaseTransaction/hooks/useTransactionStatus.tsx
+++ packages/extension/src/components/organisms/BaseTransaction/hooks/useTransactionStatus.tsx
@@ -64,7 +64,7 @@ export const useTransactionStatus = ({
               <>
                 {`Your account is not activated on the ${network} network.`}
                 <br />
-                {'Switch network or activate your account.'}
+                Switch network or activate your account.
               </>
             }
             transaction={TransactionStatus.Rejected}
@@ -97,7 +97,7 @@ export const useTransactionStatus = ({
             <>
               {errorValue}
               <br />
-              {'Please try again with a correct transaction'}
+              Please try again with a correct transaction
             </>
           }
           transaction={TransactionStatus.Rejected}
