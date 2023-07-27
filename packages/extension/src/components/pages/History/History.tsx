0ff1fd19ba7fd5d13da75a2970954fe4a3af035f
Florian Bouron <bouron.florian@yahoo.fr>
2023-07-27 18:17:57 +0200

    Add custom message for if network is not connected within the tx history

--- packages/extension/src/components/pages/History/History.tsx
+++ packages/extension/src/components/pages/History/History.tsx
@@ -1,24 +1,63 @@
 import { FC, useEffect, useState } from 'react';
 
-import { CircularProgress } from '@mui/material';
+import { Button, CircularProgress, Typography } from '@mui/material';
+import * as Sentry from '@sentry/react';
 
-import { useLedger } from '../../../contexts';
+import { LEDGER_CONNECTION_ERROR, useLedger, useNetwork } from '../../../contexts';
 import { AccountTransaction } from '../../../types';
+import { InformationMessage } from '../../molecules';
 import { TransactionListing } from '../../organisms';
 import { PageWithHeader } from '../../templates';
 
 export const History: FC = () => {
   const { getTransactions } = useLedger();
+  const { reconnectToNetwork } = useNetwork();
 
   const [transactions, setTransactions] = useState<AccountTransaction[] | null>(null);
+  const [isTxFailed, setIsTxFailed] = useState<boolean>(false);
 
   useEffect(() => {
     const getTx = async () => {
-      const tx = await getTransactions();
-      setTransactions(tx);
+      try {
+        const tx = await getTransactions();
+        setTransactions(tx);
+      } catch (e) {
+        if ((e as Error).message === LEDGER_CONNECTION_ERROR) {
+          setIsTxFailed(true);
+        } else {
+          Sentry.captureException(e);
+        }
+      }
     };
     getTx();
   }, [getTransactions]);
+
+  if (isTxFailed) {
+    return (
+      <PageWithHeader>
+        <InformationMessage
+          title="Failed to connect to the network"
+          style={{
+            padding: '15px'
+          }}
+        >
+          <Typography style={{ marginBottom: '5px' }}>
+            There was an error attempting to connect to the network. Please refresh the page and try
+            again.
+          </Typography>
+          <div style={{ textAlign: 'center', margin: '10px 0' }}>
+            <Button
+              variant="contained"
+              onClick={reconnectToNetwork}
+              style={{ marginBottom: '10px' }}
+            >
+              Refresh
+            </Button>
+          </div>
+        </InformationMessage>
+      </PageWithHeader>
+    );
+  }
 
   return (
     <PageWithHeader>
