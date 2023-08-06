65f91c1aa7cea8ac167859c815b4df470750f459
Florian Bouron <bouron.florian@yahoo.fr>
2023-08-06 15:26:17 +0200

    Add animation on the offline banner

--- packages/extension/src/components/atoms/OfflineBanner/OfflineBanner.tsx
+++ packages/extension/src/components/atoms/OfflineBanner/OfflineBanner.tsx
@@ -1,21 +1,39 @@
-import { FC } from 'react';
+import { FC, useCallback, useState } from 'react';
 
 import { WifiOff as WifiOffIcon } from '@mui/icons-material';
 import { Button, Paper, Typography } from '@mui/material';
+
+import './style.css';
 
 export interface OfflineBannerProps {
   reconnectToNetwork: () => void;
 }
 
 export const OfflineBanner: FC<OfflineBannerProps> = ({ reconnectToNetwork }) => {
+  const [isAnimating, setIsAnimating] = useState(true);
+
+  const handleRetryClick = useCallback(() => {
+    setIsAnimating(true);
+    reconnectToNetwork();
+  }, [reconnectToNetwork]);
+
+  const onAnimationEnd = useCallback(() => {
+    setIsAnimating(false);
+  }, []);
+
   return (
     <Paper elevation={24} style={{ position: 'fixed', top: 0 }}>
       <div style={{ display: 'flex', alignItems: 'center', padding: '8px' }}>
         <WifiOffIcon />
-        <Typography variant="body2" style={{ marginRight: '8px', marginLeft: '8px' }}>
+        <Typography
+          variant="body2"
+          className={isAnimating ? 'horizontal-shaking-animation' : undefined}
+          onAnimationEnd={onAnimationEnd}
+          style={{ marginRight: '8px', marginLeft: '16px' }}
+        >
           Connection failed. Please try again or switch network
         </Typography>
-        <Button variant="outlined" size="small" onClick={reconnectToNetwork}>
+        <Button variant="outlined" size="small" onClick={handleRetryClick}>
           Retry
         </Button>
       </div>
