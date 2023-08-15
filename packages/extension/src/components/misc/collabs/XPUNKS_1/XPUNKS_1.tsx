ed4bf7dce44659763cadb6bf16f90564d3bbb012
thibautbremand <thibaut.bremand@gmail.com>
2023-08-15 11:48:34 +0200

    Add feature flag

--- packages/extension/src/components/misc/collabs/XPUNKS_1/XPUNKS_1.tsx
+++ packages/extension/src/components/misc/collabs/XPUNKS_1/XPUNKS_1.tsx
@@ -1,8 +1,26 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 
 import { Button } from '@mui/material';
+import * as Sentry from '@sentry/react';
 
 export const XPUNKS_1: FC = () => {
+  const [isFeatureFlagEnabled, setIsFeatureFlagEnabled] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    fetch('featureFlags.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setIsFeatureFlagEnabled(data.FF_COLLAB_XPUNKS_1 === 1);
+      })
+      .catch((error) => {
+        Sentry.captureException(error);
+      });
+  }, []);
+
+  if (isFeatureFlagEnabled !== true) {
+    return null;
+  }
+
   const handleClick = () => {
     window.open('https://xpunks.club');
   };
