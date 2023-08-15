2a38ed07bc8374fabe550b38031a82e52be5da05
thibautbremand <thibaut.bremand@gmail.com>
2023-08-15 15:34:51 +0200

    Add animation

--- packages/extension/src/components/misc/collabs/XPUNKS_1/XPUNKS_1.tsx
+++ packages/extension/src/components/misc/collabs/XPUNKS_1/XPUNKS_1.tsx
@@ -25,6 +25,46 @@ export const XPUNKS_1: FC = () => {
     window.open('https://xpunks.club');
   };
 
+  const blinking = `
+    .blink {
+      animation: blink ease 30s infinite;
+      -webkit-animation: blink ease 30s infinite;
+      -moz-animation: blink ease 30s infinite;
+      -o-animation: blink ease 30s infinite;
+      -ms-animation: blink ease 30s infinite;
+    }
+    
+    @keyframes blink {
+      0%, 2% { opacity: 0; }
+      5%, 95% { opacity: 1; }
+      98%, 100% { opacity: 0; }
+    }
+    
+    @-moz-keyframes blink {
+      0%, 2% { opacity: 0; }
+      5%, 95% { opacity: 1; }
+      98%, 100% { opacity: 0; }
+    }
+    
+    @-webkit-keyframes blink {
+      0%, 2% { opacity: 0; }
+      5%, 95% { opacity: 1; }
+      98%, 100% { opacity: 0; }
+    }
+    
+    @-o-keyframes blink {
+      0%, 2% { opacity: 0; }
+      5%, 95% { opacity: 1; }
+      98%, 100% { opacity: 0; }
+    }
+    
+    @-ms-keyframes blink {
+      0%, 2% { opacity: 0; }
+      5%, 95% { opacity: 1; }
+      98%, 100% { opacity: 0; }
+    }
+  `;
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <Button
@@ -36,11 +76,13 @@ export const XPUNKS_1: FC = () => {
           alignItems: 'center'
         }}
       >
+        <style>{blinking}</style>
         <img
           alt="XPUNKS"
           height={40}
           src={'./collabs/XPUNKS/XPUNKS-Logo-Full-White.png'}
           style={{ marginBottom: '7px' }}
+          className={'blink'}
         />
       </Button>
     </div>
