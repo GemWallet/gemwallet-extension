3cb1f2986e00432321cebe181422e16586abe6cf
thibautbremand <thibaut.bremand@gmail.com>
2023-08-17 21:55:07 +0200

    Fix post rebase

--- packages/extension/src/constants/storage.ts
+++ packages/extension/src/constants/storage.ts
@@ -5,6 +5,8 @@ export const STORAGE_NETWORK = 'network';
 export const STORAGE_PERMISSION_SUBMIT_BULK = 'permissionSubmitBulkTransactions';
 export const STORAGE_REMEMBER_SESSION = 'rememberSession';
 export const STORAGE_SELECTED_WALLET = 'selectedWallet';
+export const STORAGE_TRUSTED_APPS = 'trustedApps';
+export const STORAGE_WALLETS = 'wallets';
 
 // TTL
 export const TTL_FEATURE_FLAGS = 60 * 60 * 1000; // 1 hour
