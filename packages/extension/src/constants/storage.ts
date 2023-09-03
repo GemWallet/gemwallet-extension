923dcd5ffa48887732d4930ef4c9e6c6e29135f0
Florian Bouron <bouron.florian@yahoo.fr>
2023-09-03 21:21:22 +0100

    Remove the XPUNKS collab

--- packages/extension/src/constants/storage.ts
+++ packages/extension/src/constants/storage.ts
@@ -1,6 +1,5 @@
 // Keys
 export const STORAGE_CUSTOM_NETWORKS = 'customNetworks';
-export const STORAGE_FEATURE_FLAGS = 'featureFlags';
 export const STORAGE_NETWORK = 'network';
 export const STORAGE_PERMISSION_SUBMIT_BULK = 'permissionSubmitBulkTransactions';
 export const STORAGE_REMEMBER_SESSION = 'rememberSession';
@@ -8,6 +7,3 @@ export const STORAGE_SELECTED_WALLET = 'selectedWallet';
 export const STORAGE_STATE_TRANSACTION = 'hasTxInProgress';
 export const STORAGE_TRUSTED_APPS = 'trustedApps';
 export const STORAGE_WALLETS = 'wallets';
-
-// TTL
-export const TTL_FEATURE_FLAGS = 60 * 60 * 1000; // 1 hour
