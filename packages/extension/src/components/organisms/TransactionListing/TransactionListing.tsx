f902d9fa20f73ad4fb34d016b3275dc4c7a55313
thibautbremand <thibaut.bremand@gmail.com>
2023-09-23 19:13:13 +0200

    Add transactions filter

--- packages/extension/src/components/organisms/TransactionListing/TransactionListing.tsx
+++ packages/extension/src/components/organisms/TransactionListing/TransactionListing.tsx
@@ -2,9 +2,13 @@ import React, { FC, useCallback, useState } from 'react';
 
 import CloseIcon from '@mui/icons-material/Close';
 import TransactionIcon from '@mui/icons-material/CompareArrows';
+import FilterListIcon from '@mui/icons-material/FilterList';
 import {
   AppBar,
   Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
   Divider,
   IconButton,
   List,
@@ -16,6 +20,7 @@ import {
   Toolbar,
   Typography
 } from '@mui/material';
+import { Button, FormControlLabel, Checkbox } from '@mui/material';
 import { TransitionProps } from '@mui/material/transitions';
 import { unix } from 'moment';
 import { convertHexToString, DepositPreauth, dropsToXrp, Payment, SetRegularKey } from 'xrpl';
@@ -40,7 +45,8 @@ const Transition = React.forwardRef(function Transition(
   },
   ref: React.Ref<unknown>
 ) {
-  return <Slide direction="up" ref={ref} {...props} />;
+  const { children, ...otherProps } = props;
+  return <Slide direction="up" ref={ref} {...otherProps} children={children} />;
 });
 
 type TransactionFormatter = (transaction: AccountTransaction, publicAddress: string) => string;
@@ -115,12 +121,15 @@ const formatDate = (unixTimestamp: number): string => {
 };
 
 export const TransactionListing: FC<TransactionListingProps> = ({ transactions }) => {
-  const { getCurrentWallet } = useWallet();
-
   const [tx, setTx] = useState<ExtendedAccountTransaction[]>(
     transactions.length > 0 ? transactions.map((t) => ({ ...t, touched: false })) : []
   );
+  const [filterTypes, setFilterTypes] = useState<string[]>([]);
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+
+  const { getCurrentWallet } = useWallet();
   const wallet = getCurrentWallet();
+  const transactionTypes = [...new Set(transactions.map((tx) => tx.tx?.TransactionType))].sort();
 
   const handleClick = useCallback(
     (index: number) => {
@@ -172,169 +181,220 @@ export const TransactionListing: FC<TransactionListingProps> = ({ transactions }
     );
   }
 
+  const filteredTransactions = filterTypes.length
+    ? tx.filter((tx) => filterTypes.includes(tx.tx?.TransactionType || ''))
+    : tx;
+
   return (
-    <List dense style={{ paddingTop: 0 }}>
-      {Array.from(groupTransactionsByDate(tx)).map(([date, transactionsForDate]) => (
-        <div key={date}>
-          <Typography
-            style={{ marginTop: '0', marginBottom: '5px', fontSize: '14px', fontWeight: '400' }}
-          >
-            {date}
-          </Typography>
-          {transactionsForDate.map((transaction, index) => (
-            <div key={transaction.tx?.hash ?? index}>
-              <Paper
-                elevation={5}
-                style={{
-                  padding: '10px',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'space-between',
-                  marginBottom: '10px',
-                  cursor: 'pointer'
-                }}
-                onClick={() => handleClick(index)}
-              >
-                <ListItem>
-                  <ListItemIcon>
-                    <TransactionIcon />
-                  </ListItemIcon>
-                  <ListItemText
-                    primary={formatTransaction(transaction, wallet.publicAddress)}
-                    secondary={transaction.tx?.date ? formatDate(transaction.tx?.date) : undefined}
-                  />
-                </ListItem>
-              </Paper>
-              <Dialog
-                open={transaction.touched}
-                TransitionComponent={Transition}
-                fullScreen
-                data-testid="dialog"
-              >
-                <AppBar sx={{ position: 'relative' }}>
-                  <Toolbar>
-                    <IconButton
-                      edge="start"
-                      color="inherit"
-                      aria-label="close"
-                      onClick={() => handleClose(index)}
-                      style={{ cursor: 'pointer' }}
-                      data-testid="close-button"
-                    >
-                      <CloseIcon />
-                    </IconButton>
-                    <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="p">
-                      Transaction Details
-                    </Typography>
-                  </Toolbar>
-                </AppBar>
-                <List sx={{ width: '100%', wordBreak: 'break-word' }}>
-                  <ListItem style={{ padding: '8px 24px' }}>
-                    <ListItemText primary="Account" secondary={transaction.tx?.Account} />
-                  </ListItem>
-                  <Divider light />
-                  {renderDestinationField(transaction)}
-                  <Divider light />
-                  <ListItem style={{ padding: '8px 24px' }}>
-                    <ListItemText
-                      primary="Transaction"
-                      secondary={formatTransaction(transaction, wallet.publicAddress)}
-                    />
-                  </ListItem>
-                  <Divider light />
-                  <ListItem style={{ padding: '8px 24px' }}>
-                    <ListItemText
-                      primary="Fees"
-                      secondary={dropsToXrp(Number(transaction.tx?.Fee))}
-                    />
-                  </ListItem>
-                  <Divider light />
-                  <ListItem style={{ padding: '8px 24px' }}>
-                    <ListItemText
-                      primary="Date"
-                      secondary={
-                        transaction.tx?.date ? formatDate(transaction.tx?.date) : undefined
+    <>
+      <div style={{ position: 'relative', margin: '0' }}>
+        <IconButton onClick={() => setDialogOpen(true)} style={{ float: 'right' }} size="small">
+          <FilterListIcon style={{ fontSize: '18px' }} />
+        </IconButton>
+        <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+          <DialogTitle>Filter Transactions</DialogTitle>
+          <DialogContent>
+            {transactionTypes.map((type) => (
+              <FormControlLabel
+                key={type}
+                control={
+                  <Checkbox
+                    color="primary"
+                    value={type}
+                    checked={filterTypes.includes(type as string)}
+                    onChange={(e) => {
+                      const checked = e.target.checked;
+                      if (type) {
+                        setFilterTypes((prev) =>
+                          checked ? [...prev, type] : prev.filter((t) => t !== type)
+                        );
                       }
-                    />
-                  </ListItem>
-                  <Divider light />
-                  {transaction.tx?.Memos?.[0]?.Memo?.MemoData ? (
-                    <>
+                    }}
+                  />
+                }
+                label={type}
+              />
+            ))}
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setDialogOpen(false)} color="primary">
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+      <List dense style={{ paddingTop: 0 }}>
+        {Array.from(groupTransactionsByDate(filteredTransactions)).map(
+          ([date, transactionsForDate]) => (
+            <div key={date}>
+              <Typography
+                style={{ marginTop: '0', marginBottom: '5px', fontSize: '14px', fontWeight: '400' }}
+              >
+                {date}
+              </Typography>
+              {transactionsForDate.map((transaction, index) => (
+                <div key={transaction.tx?.hash ?? index}>
+                  <Paper
+                    elevation={5}
+                    style={{
+                      padding: '10px',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'space-between',
+                      marginBottom: '10px',
+                      cursor: 'pointer'
+                    }}
+                    onClick={() => handleClick(index)}
+                  >
+                    <ListItem>
+                      <ListItemIcon>
+                        <TransactionIcon />
+                      </ListItemIcon>
+                      <ListItemText
+                        primary={formatTransaction(transaction, wallet.publicAddress)}
+                        secondary={
+                          transaction.tx?.date ? formatDate(transaction.tx?.date) : undefined
+                        }
+                      />
+                    </ListItem>
+                  </Paper>
+                  <Dialog
+                    open={transaction.touched}
+                    TransitionComponent={Transition}
+                    fullScreen
+                    data-testid="dialog"
+                  >
+                    <AppBar sx={{ position: 'relative' }}>
+                      <Toolbar>
+                        <IconButton
+                          edge="start"
+                          color="inherit"
+                          aria-label="close"
+                          onClick={() => handleClose(index)}
+                          style={{ cursor: 'pointer' }}
+                          data-testid="close-button"
+                        >
+                          <CloseIcon />
+                        </IconButton>
+                        <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="p">
+                          Transaction Details
+                        </Typography>
+                      </Toolbar>
+                    </AppBar>
+                    <List sx={{ width: '100%', wordBreak: 'break-word' }}>
+                      <ListItem style={{ padding: '8px 24px' }}>
+                        <ListItemText primary="Account" secondary={transaction.tx?.Account} />
+                      </ListItem>
+                      <Divider light />
+                      {renderDestinationField(transaction)}
+                      <Divider light />
                       <ListItem style={{ padding: '8px 24px' }}>
                         <ListItemText
-                          primary="Memo"
-                          secondary={convertHexToString(transaction.tx?.Memos?.[0]?.Memo?.MemoData)}
+                          primary="Transaction"
+                          secondary={formatTransaction(transaction, wallet.publicAddress)}
                         />
                       </ListItem>
                       <Divider light />
-                    </>
-                  ) : null}
-                  {transaction.meta &&
-                  typeof transaction.meta === 'object' &&
-                  'nftoken_id' in transaction.meta ? (
-                    <>
                       <ListItem style={{ padding: '8px 24px' }}>
                         <ListItemText
-                          primary="NFT Token ID"
-                          secondary={(transaction.meta as any).nftoken_id}
+                          primary="Fees"
+                          secondary={dropsToXrp(Number(transaction.tx?.Fee))}
                         />
                       </ListItem>
                       <Divider light />
-                    </>
-                  ) : null}
-                  {transaction.meta &&
-                  typeof transaction.meta === 'object' &&
-                  'offer_id' in transaction.meta ? (
-                    <>
                       <ListItem style={{ padding: '8px 24px' }}>
                         <ListItemText
-                          primary="Offer ID"
-                          secondary={(transaction.meta as any).offer_id}
+                          primary="Date"
+                          secondary={
+                            transaction.tx?.date ? formatDate(transaction.tx?.date) : undefined
+                          }
                         />
                       </ListItem>
                       <Divider light />
-                    </>
-                  ) : null}
-                  <ListItem style={{ padding: '8px 24px' }}>
-                    <ListItemText primary="Transaction Hash" secondary={transaction.tx?.hash} />
-                  </ListItem>
-                  <Divider light />
-                  {transaction.tx &&
-                  'DestinationTag' in transaction.tx &&
-                  transaction.tx?.DestinationTag ? (
-                    <>
+                      {transaction.tx?.Memos?.[0]?.Memo?.MemoData ? (
+                        <>
+                          <ListItem style={{ padding: '8px 24px' }}>
+                            <ListItemText
+                              primary="Memo"
+                              secondary={convertHexToString(
+                                transaction.tx?.Memos?.[0]?.Memo?.MemoData
+                              )}
+                            />
+                          </ListItem>
+                          <Divider light />
+                        </>
+                      ) : null}
+                      {transaction.meta &&
+                      typeof transaction.meta === 'object' &&
+                      'nftoken_id' in transaction.meta ? (
+                        <>
+                          <ListItem style={{ padding: '8px 24px' }}>
+                            <ListItemText
+                              primary="NFT Token ID"
+                              secondary={(transaction.meta as any).nftoken_id}
+                            />
+                          </ListItem>
+                          <Divider light />
+                        </>
+                      ) : null}
+                      {transaction.meta &&
+                      typeof transaction.meta === 'object' &&
+                      'offer_id' in transaction.meta ? (
+                        <>
+                          <ListItem style={{ padding: '8px 24px' }}>
+                            <ListItemText
+                              primary="Offer ID"
+                              secondary={(transaction.meta as any).offer_id}
+                            />
+                          </ListItem>
+                          <Divider light />
+                        </>
+                      ) : null}
                       <ListItem style={{ padding: '8px 24px' }}>
-                        <ListItemText
-                          primary="Destination Tag"
-                          secondary={transaction.tx?.DestinationTag}
-                        />
+                        <ListItemText primary="Transaction Hash" secondary={transaction.tx?.hash} />
                       </ListItem>
                       <Divider light />
-                    </>
-                  ) : null}
-                  {transaction.tx && 'Flags' in transaction.tx && transaction.tx?.Flags ? (
-                    <>
+                      {transaction.tx &&
+                      'DestinationTag' in transaction.tx &&
+                      transaction.tx?.DestinationTag ? (
+                        <>
+                          <ListItem style={{ padding: '8px 24px' }}>
+                            <ListItemText
+                              primary="Destination Tag"
+                              secondary={transaction.tx?.DestinationTag}
+                            />
+                          </ListItem>
+                          <Divider light />
+                        </>
+                      ) : null}
+                      {transaction.tx && 'Flags' in transaction.tx && transaction.tx?.Flags ? (
+                        <>
+                          <ListItem style={{ padding: '8px 24px' }}>
+                            <ListItemText
+                              primary="Flags"
+                              secondary={formatFlagsToNumber(transaction.tx)}
+                            />
+                          </ListItem>
+                          <Divider light />
+                        </>
+                      ) : null}
                       <ListItem style={{ padding: '8px 24px' }}>
                         <ListItemText
-                          primary="Flags"
-                          secondary={formatFlagsToNumber(transaction.tx)}
+                          primary="Ledger Index"
+                          secondary={transaction.tx?.ledger_index}
                         />
                       </ListItem>
-                      <Divider light />
-                    </>
-                  ) : null}
-                  <ListItem style={{ padding: '8px 24px' }}>
-                    <ListItemText primary="Ledger Index" secondary={transaction.tx?.ledger_index} />
-                  </ListItem>
-                  <ListItem style={{ padding: '8px 24px' }}>
-                    <ListItemText primary="Sequence" secondary={transaction.tx?.Sequence} />
-                  </ListItem>
-                </List>
-              </Dialog>
+                      <ListItem style={{ padding: '8px 24px' }}>
+                        <ListItemText primary="Sequence" secondary={transaction.tx?.Sequence} />
+                      </ListItem>
+                    </List>
+                  </Dialog>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      ))}
-    </List>
+          )
+        )}
+      </List>
+    </>
   );
 };
