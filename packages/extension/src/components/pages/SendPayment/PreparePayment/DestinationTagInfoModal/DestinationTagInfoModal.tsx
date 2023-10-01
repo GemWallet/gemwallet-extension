43723d60056a1959d2bc0e3c9b38d12b4c7a981e
Thibaut Bremand <thibaut.bremand@gmail.com>
2023-10-01 20:00:07 +0200

    Add infos about destination tag in the Send Payment view (#297)
    * Add infos about destination tag in the Send Payment view
    
    * Rename 'Memo' into 'Notes' in Send Payment view
    
    To prevent confusion with destination tag
    
    * Remove bigger font on 'must' keyword
    
    * Simplify DestinationTagInfo Modal
    
    * Add separation between paragraphs in DestinationTagInfoModal view
    
    ---------
    
    Co-authored-by: Florian Bouron <bouron.florian@yahoo.fr>

---
+++ packages/extension/src/components/pages/SendPayment/PreparePayment/DestinationTagInfoModal/DestinationTagInfoModal.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react';
+
+import {
+  Dialog,
+  DialogActions,
+  Button,
+  DialogTitle,
+  DialogContent,
+  DialogContentText
+} from '@mui/material';
+
+export interface DestinationTagInfoModalProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+export const DestinationTagInfoModal: FC<DestinationTagInfoModalProps> = ({ open, onClose }) => {
+  return (
+    <Dialog open={open} onClose={onClose} scroll="paper">
+      <DialogTitle>About the Destination Tag</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          When you're sending funds to an exchange, it is crucial to include a destination tag. A
+          destination tag is a numeric value that will associate your transaction with your user
+          account on the exchange. Some exchanges call it "memo".
+        </DialogContentText>
+        <DialogContentText style={{ paddingTop: '16px' }}>
+          Failing to include a destination tag can result in the loss of your funds, as the exchange
+          won't be able to associate the incoming funds with your user account. Always verify the
+          destination tag given by the exchange and enter it correctly before confirming the
+          transaction.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="primary">
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
