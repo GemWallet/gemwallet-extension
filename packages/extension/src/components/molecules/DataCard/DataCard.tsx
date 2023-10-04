b86b0acf0c1cd2eca6c24c09a17fcf0828be5925
thibautbremand <thibaut.bremand@gmail.com>
2023-10-04 23:31:38 +0200

    Remove obsolete code

--- packages/extension/src/components/molecules/DataCard/DataCard.tsx
+++ packages/extension/src/components/molecules/DataCard/DataCard.tsx
@@ -1,4 +1,4 @@
-import { FC, forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
@@ -15,96 +15,92 @@ interface DataCardProps {
   paddingTop?: number;
 }
 
-export const DataCard: FC<DataCardProps> = forwardRef(
-  (
-    { formattedData, dataName, isExpanded, setIsExpanded, thresholdHeight = 120, paddingTop = 30 },
-    ref
-  ) => {
-    const messageBoxRef = useRef<HTMLDivElement>(null);
-    const [isExpandable, setIsExpandable] = useState(false);
+export const DataCard: FC<DataCardProps> = ({
+  formattedData,
+  dataName,
+  isExpanded,
+  setIsExpanded,
+  thresholdHeight = 120,
+  paddingTop = 30
+}) => {
+  const messageBoxRef = useRef<HTMLDivElement>(null);
+  const [isExpandable, setIsExpandable] = useState(false);
 
-    useImperativeHandle(ref, () => ({
-      getMessageBoxHeight: () => {
-        return messageBoxRef.current ? messageBoxRef.current.offsetHeight : 0;
-      }
-    }));
+  useEffect(() => {
+    if (messageBoxRef.current && messageBoxRef.current.offsetHeight > thresholdHeight) {
+      setIsExpandable(true);
+    } else {
+      setIsExpandable(false);
+    }
+  }, [formattedData, thresholdHeight]);
 
-    useEffect(() => {
-      if (messageBoxRef.current && messageBoxRef.current.offsetHeight > thresholdHeight) {
-        setIsExpandable(true);
-      } else {
-        setIsExpandable(false);
-      }
-    }, [formattedData, thresholdHeight]);
-
-    return (
-      <Paper
-        elevation={24}
+  return (
+    <Paper
+      elevation={24}
+      style={{
+        padding: '15px',
+        marginTop: `${paddingTop}px`,
+        borderRadius: '15px',
+        backgroundColor: '#000000'
+      }}
+    >
+      <div
         style={{
-          padding: '15px',
-          marginTop: `${paddingTop}px`,
-          borderRadius: '15px',
-          backgroundColor: '#000000'
+          display: 'flex',
+          alignItems: 'center',
+          marginLeft: isExpandable ? '-12px' : '0'
         }}
       >
-        <div
+        {isExpandable ? (
+          <IconButton data-cy="expandButton" onClick={() => setIsExpanded(!isExpanded)}>
+            {isExpanded ? (
+              <ExpandMoreIcon style={{ fontSize: '20px' }} />
+            ) : (
+              <ChevronRightIcon style={{ fontSize: '20px' }} />
+            )}
+          </IconButton>
+        ) : null}
+        {dataName ? <Typography variant="body1">{dataName}</Typography> : null}
+      </div>
+      <div
+        ref={messageBoxRef}
+        style={{
+          position: 'relative',
+          overflowY: isExpanded ? 'auto' : 'hidden',
+          maxHeight: isExpanded ? 'none' : `${thresholdHeight}px`,
+          borderRadius: '10px',
+          paddingBottom: '4px'
+        }}
+      >
+        <pre
           style={{
-            display: 'flex',
-            alignItems: 'center',
-            marginLeft: isExpandable ? '-12px' : '0'
-          }}
-        >
-          {isExpandable ? (
-            <IconButton data-cy="expandButton" onClick={() => setIsExpanded(!isExpanded)}>
-              {isExpanded ? (
-                <ExpandMoreIcon style={{ fontSize: '20px' }} />
-              ) : (
-                <ChevronRightIcon style={{ fontSize: '20px' }} />
-              )}
-            </IconButton>
-          ) : null}
-          {dataName ? <Typography variant="body1">{dataName}</Typography> : null}
-        </div>
-        <div
-          ref={messageBoxRef}
-          style={{
-            position: 'relative',
-            overflowY: isExpanded ? 'auto' : 'hidden',
-            maxHeight: isExpanded ? 'none' : `${thresholdHeight}px`,
+            color: SECONDARY_GRAY,
+            whiteSpace: 'pre-wrap',
+            overflowWrap: 'break-word',
             borderRadius: '10px',
-            paddingBottom: '4px'
+            marginTop: '8px',
+            marginBottom: '0',
+            fontFamily: 'inherit', // To keep the font consistent with the rest of the design
+            fontSize: '15px'
           }}
         >
-          <pre
+          {formattedData}
+        </pre>
+        {!isExpanded && isExpandable ? (
+          <div
             style={{
-              color: SECONDARY_GRAY,
-              whiteSpace: 'pre-wrap',
-              overflowWrap: 'break-word',
-              borderRadius: '10px',
-              marginTop: '8px',
-              marginBottom: '0',
-              fontFamily: 'inherit', // To keep the font consistent with the rest of the design
-              fontSize: '15px'
+              content: '',
+              display: 'block',
+              position: 'absolute',
+              bottom: '0',
+              left: '0',
+              right: '0',
+              height: '20px',
+              backgroundImage: 'linear-gradient(to top, rgba(40, 40, 40), rgba(0, 0, 0, 0))'
             }}
-          >
-            {formattedData}
-          </pre>
-          {!isExpanded && isExpandable ? (
-            <div
-              style={{
-                content: '',
-                display: 'block',
-                position: 'absolute',
-                bottom: '0',
-                left: '0',
-                right: '0',
-                height: '20px',
-                backgroundImage: 'linear-gradient(to top, rgba(40, 40, 40), rgba(0, 0, 0, 0))'
-              }}
-            />
-          ) : null}
-        </div>
-      </Paper>
-    );
-  }
-);
+          />
+        ) : null}
+      </div>
+    </Paper>
+  );
+};
