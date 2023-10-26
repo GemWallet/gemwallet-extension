341bb200b16235dc87c11e0cb9f7b0a812e77fea
Florian Bouron <bouron.florian@yahoo.fr>
2023-10-26 18:23:21 +0200

    Edit webpack configuration

--- packages/extension/craco.config.js
+++ packages/extension/craco.config.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const webpack = require("webpack");
 
 const extensionDirectory = fs.realpathSync(process.cwd());
 const apiDirectory = path.resolve(extensionDirectory, '../api');
@@ -37,14 +38,43 @@ module.exports = {
       };
 
       if (env === 'production') {
-        return {
+        webpackConfig = {
           ...webpackConfig,
           ...chromeServices
         };
+      } else {
+        webpackConfig = {
+          ...webpackConfig
+        };
       }
-      return {
-        ...webpackConfig
-      };
+
+      const fallback = webpackConfig.resolve.fallback || {};
+      Object.assign(fallback, {
+        assert: require.resolve("assert"),
+        crypto: require.resolve("crypto-browserify"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        os: require.resolve("os-browserify"),
+        stream: require.resolve("stream-browserify"),
+        url: require.resolve("url"),
+        ws: require.resolve("xrpl/dist/npm/client/WSWrapper"),
+      });
+
+      webpackConfig.resolve.fallback = fallback;
+      webpackConfig.plugins = (webpackConfig.plugins || []).concat([
+        new webpack.ProvidePlugin({
+          process: "process/browser",
+          Buffer: ["buffer", "Buffer"],
+        }),
+      ]);
+
+      // This is deprecated in webpack 5 but alias false does not seem to work
+      webpackConfig.module.rules.push({
+        test: /node_modules[\\\/]https-proxy-agent[\\\/]/,
+        use: "null-loader",
+      });
+
+      return webpackConfig;
     }
   }
 };
