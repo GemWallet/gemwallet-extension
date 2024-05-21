587f6c20a137cac557defb651b3c5e38a60d786e
Florian Bouron <bouron.florian@yahoo.fr>
2024-05-21 19:17:53 +0200

    Cleanup the receive flow

--- packages/extension/cypress/e2e/receive.cy.ts
+++ packages/extension/cypress/e2e/receive.cy.ts
@@ -1,16 +1,14 @@
 import { Chain, XRPLNetwork } from '@gemwallet/constants';
+import { navigate } from '../utils/navigation';
+
+const PASSWORD = Cypress.env('password');
+const LOCAL_STORAGE_WALLETS = Cypress.env('localStorage');
 
 describe('ReceivePayment View', () => {
   beforeEach(() => {
-    // deepcode ignore NoHardcodedPasswords: password used for testing purposes
-    const PASSWORD = 'SECRET_PASSWORD';
-
     // Mock the localStorage with a wallet already loaded
     cy.window().then((win) => {
-      win.localStorage.setItem(
-        'wallets',
-        'U2FsdGVkX1/JAHkPXi6ZonxQukDVxSlHcveDl30pBLB/Y9tPmrtAIi0ulBoj+mgxv/qj29Odgw8pRm0QzwGpT5zKmTRyf4QHmjpl5UcSNfRw95l/ZxPMgOpy/qrOJgWQHKNVK1TUpQHuV/c+ULBmpyOeMsI60paKXKvBEddTfHggacZV9ABvmCZZbIMM4h3tRn0HtVQY8kFvp9yJuUxq8T6BMgHzCys7hzUjtwdZ81YVEfdUShzEkleuqLsx4STqNvibUmf6HnwCgMCCPTzjulr3XvZK+yfreBb3RPsivSCsl5dwSz0ORtNwg04zjLiTvR+1btv91kifEBrMo3dh/Q=='
-      );
+      win.localStorage.setItem('wallets', LOCAL_STORAGE_WALLETS);
       win.localStorage.setItem(
         'network',
         JSON.stringify({
@@ -19,37 +17,7 @@ describe('ReceivePayment View', () => {
         })
       );
     });
-    cy.visit('http://localhost:3000/', {
-      onBeforeLoad(win) {
-        (win as any).chrome = (win as any).chrome || {};
-        (win as any).chrome.runtime = {
-          sendMessage(message, cb) {}
-        };
-
-        (win as any).chrome.storage = {
-          local: {
-            get(key, cb) {},
-            set(obj, cb) {
-              if (cb) cb();
-            }
-          },
-          session: {
-            get(key, cb) {},
-            set(obj, cb) {
-              if (cb) cb();
-            }
-          }
-        };
-
-        cy.stub((win as any).chrome.runtime, 'sendMessage').resolves({});
-      }
-    });
-
-    cy.get('.MuiCircularProgress-root', { timeout: 20000 }).should('not.exist');
-
-    // Login
-    cy.get('input[name="password"]').type(PASSWORD);
-    cy.contains('button', 'Unlock').click();
+    navigate('http://localhost:3000/', PASSWORD);
 
     // Go to the receive page
     cy.contains('button', 'Receive').click();
@@ -58,6 +26,6 @@ describe('ReceivePayment View', () => {
   it('should display the QRCode and wallet address', () => {
     cy.get('canvas').should('be.visible');
 
-    cy.get('[data-testid="receive-payment-address"]').should('have.text', 'rB3J...7HQu');
+    cy.get('[data-testid="receive-payment-address"]').should('have.text', 'rpeV...7gHu');
   });
 });
