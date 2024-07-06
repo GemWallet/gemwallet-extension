d90fae3627c86cc8f0fef3756bcc05589ecaf1b3
Florian Bouron <bouron.florian@yahoo.fr>
2024-07-06 15:28:20 +0200

    Fix some more linting issues

--- packages/extension/src/utils/storageLocal.ts
+++ packages/extension/src/utils/storageLocal.ts
@@ -3,19 +3,7 @@
  */
 
 export const saveData = (key: string, value: string) => {
-  try {
-    localStorage.setItem(key, value);
-  } catch (e) {
-    /*
-     * If localStorage is disabled or blocked by the browser,
-     * a SecurityError exception will be thrown when trying to access localStorage.
-     * If the key or value argument passed to localStorage.setItem() is not a string,
-     * a TypeError exception will be thrown.
-     * If the user has exceeded the storage quota for localStorage,
-     * a QuotaExceededError exception will be thrown.
-     */
-    throw e;
-  }
+  localStorage.setItem(key, value);
 };
 
 export const loadData = (key: string) => {
@@ -35,15 +23,5 @@ export const loadData = (key: string) => {
 };
 
 export const removeData = (key: string) => {
-  try {
-    localStorage.removeItem(key);
-  } catch (e) {
-    /*
-     * If localStorage is disabled or blocked by the browser,
-     * a SecurityError exception will be thrown when trying to access localStorage.
-     * If the key argument passed to localStorage.removeItem() is not a string,
-     * a TypeError exception will be thrown.
-     */
-    throw e;
-  }
+  localStorage.removeItem(key);
 };
