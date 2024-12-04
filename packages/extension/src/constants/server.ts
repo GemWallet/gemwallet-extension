179c239af80487f06fd7448aa97a2c8b5f497a7b
Thibaut Bremand <thibaut.bremand@gmail.com>
2024-12-04 19:13:51 +0100

    Update owner reserve

--- packages/extension/src/constants/server.ts
+++ packages/extension/src/constants/server.ts
@@ -1,3 +1,3 @@
-export const DEFAULT_RESERVE = 10;
-export const RESERVE_PER_OWNER = 2;
+export const DEFAULT_RESERVE = 1;
+export const RESERVE_PER_OWNER = 0.2;
 export const XAHAU_RESERVE_PER_OWNER = 0.2;
