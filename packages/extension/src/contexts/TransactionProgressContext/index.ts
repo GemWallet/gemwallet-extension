d92b39e6909f9e7f7f5f9763d6bc7076ed614475
Benaor <benjamingirard25@gmail.com>
2024-12-29 13:10:16 +0100

    extract useTransactionProgress in it own file

--- packages/extension/src/contexts/TransactionProgressContext/index.ts
+++ packages/extension/src/contexts/TransactionProgressContext/index.ts
@@ -1,1 +1,6 @@
-export * from './TransactionProgressContext';
+export {
+  TransactionProgressContext,
+  TransactionProgressProvider
+} from './TransactionProgressContext';
+export { TransactionProgressStatus } from './TransactionProgressStatus.enum';
+export { useTransactionProgress } from './useTransactionProgress';
